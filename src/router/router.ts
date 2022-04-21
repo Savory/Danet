5f236571ea975859367988a176941918c8f84eee
Thomas Cruveilher <thomas@fyndl.io>
2022-04-21 22:51:34 +0200

    feature(router): handleRoute is now a method

--- src/router/router.ts
+++ src/router/router.ts
@@ -13,25 +13,6 @@ type Callback = (
 
 type HttpContext = Context;
 
-function handleRoute(callback: Callback) {
-  return async (context: HttpContext) => {
-    try {
-      const response = await callback(context);
-      context.response.body = response;
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Internal server error!";
-
-      context.response.body = {
-        ...error,
-        status,
-        message,
-      };
-      context.response.status = status;
-    }
-  };
-}
-
 export class DeNestRouter {
   public router = new Router()
   methodsMap = new Map([
@@ -62,6 +43,25 @@ export class DeNestRouter {
     if (!routerFn)
       throw new Error(`The method ${httpMethod} can not be handled by.`);
 
-    routerFn.call(this.router, path, handleRoute(method));
+    routerFn.call(this.router, path, this.handleRoute(method));
+  }
+
+  handleRoute(ControllerMethod: Callback) {
+    return async (context: HttpContext) => {
+      try {
+        const response = await ControllerMethod(context);
+        context.response.body = response;
+      } catch (error) {
+        const status = error.status || 500;
+        const message = error.message || "Internal server error!";
+
+        context.response.body = {
+          ...error,
+          status,
+          message,
+        };
+        context.response.status = status;
+      }
+    };
   }
 }
