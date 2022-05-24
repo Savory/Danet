0da70185f81d9b55b03d2ba719b663a6952fc651
Thomas Cruveilher <thomas@fyndl.io>
2022-05-25 00:21:19 +0200

    refactor(http-exception): set name to constructor.name

--- src/exception/http/httpException.ts
+++ src/exception/http/httpException.ts
@@ -4,32 +4,27 @@ export class HttpException extends Error {
 	constructor(
 		readonly statusCode: number,
 		readonly description: string,
-		readonly name = 'HttpException',
 	) {
 		super(`${statusCode} - ${description}`);
-		this.name = name;
-		Object.setPrototypeOf(this, HttpException.prototype);
+		this.name = this.constructor.name;
 	}
 }
 
 export class ForbiddenException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.FORBIDDEN, 'Forbidden', 'ForbiddenException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.FORBIDDEN, 'Forbidden');
 	}
 }
 
 export class BadRequestException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.BAD_REQUEST, 'Bad request', 'BadRequestException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.BAD_REQUEST, 'Bad request');
 	}
 }
 
 export class UnauthorizedException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.UNAUTHORIZED, 'Unauthorized', 'UnauthorizedException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.UNAUTHORIZED, 'Unauthorized');
 	}
 }
 
@@ -38,16 +33,13 @@ export class PaymentRequiredException extends HttpException {
 		super(
 			HTTP_STATUS.PAYMENT_REQUIRED,
 			'Payment required',
-			'PaymentRequiredException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
 export class NotFoundException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.NOT_FOUND, 'Not found', 'NotFoundException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.NOT_FOUND, 'Not found');
 	}
 }
 
@@ -56,9 +48,7 @@ export class MethodNotAllowedException extends HttpException {
 		super(
 			HTTP_STATUS.METHOD_NOT_ALLOWED,
 			'Method not allowed',
-			'MethodNotAllowedException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -67,9 +57,7 @@ export class NotAcceptableException extends HttpException {
 		super(
 			HTTP_STATUS.NOT_ACCEPTABLE,
 			'Not acceptable',
-			'NotAcceptableException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -78,9 +66,7 @@ export class ProxyAuthenticationRequiredException extends HttpException {
 		super(
 			HTTP_STATUS.PROXY_AUTHENTICATION_REQUIRED,
 			'Proxy authentication required',
-			'ProxyAuthenticationRequiredException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -89,23 +75,19 @@ export class RequestTimeoutException extends HttpException {
 		super(
 			HTTP_STATUS.REQUEST_TIMEOUT,
 			'Request timeout',
-			'RequestTimeoutException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
 export class ConflictException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.CONFLICT, 'Conflict', 'ConflictException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.CONFLICT, 'Conflict');
 	}
 }
 
 export class GoneException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.GONE, 'Gone', 'GoneException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.GONE, 'Gone');
 	}
 }
 
@@ -114,9 +96,7 @@ export class LengthRequiredException extends HttpException {
 		super(
 			HTTP_STATUS.LENGTH_REQUIRED,
 			'Length required',
-			'LengthRequiredException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -125,9 +105,7 @@ export class PreconditionFailedException extends HttpException {
 		super(
 			HTTP_STATUS.PRECONDITION_FAILED,
 			'Precondition failed',
-			'PreconditionFailedException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -136,16 +114,13 @@ export class PayloadTooLargeException extends HttpException {
 		super(
 			HTTP_STATUS.PAYLOAD_TOO_LARGE,
 			'Payload too large',
-			'PayloadTooLargeException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
 export class URITooLongException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.URI_TOO_LONG, 'URI too long', 'URITooLongException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.URI_TOO_LONG, 'URI too long');
 	}
 }
 
@@ -154,9 +129,7 @@ export class UnsupportedMediaTypeException extends HttpException {
 		super(
 			HTTP_STATUS.UNSUPPORTED_MEDIA_TYPE,
 			'Unsupported media type',
-			'UnsupportedMediaTypeException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -165,9 +138,7 @@ export class RequestedRangeNotSatisfiableException extends HttpException {
 		super(
 			HTTP_STATUS.REQUESTED_RANGE_NOT_SATISFIABLE,
 			'Requested range not statisfiable',
-			'RequestedRangeNotSatisfiableException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -176,23 +147,19 @@ export class ExpectationFailedException extends HttpException {
 		super(
 			HTTP_STATUS.EXPECTATION_FAILED,
 			'Expectation failed',
-			'ExpectationFailedException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
 export class IAmATeapotException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.I_AM_A_TEAPOT, 'I am a teapot', 'IAmATeapotException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.I_AM_A_TEAPOT, 'I am a teapot');
 	}
 }
 
 export class MisdirectedException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.MISDIRECTED, 'Misdirected', 'MisdirectedException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.MISDIRECTED, 'Misdirected');
 	}
 }
 
@@ -201,9 +168,7 @@ export class UnprocessableEntityException extends HttpException {
 		super(
 			HTTP_STATUS.UNPROCESSABLE_ENTITY,
 			'Unprocessable entity',
-			'UnprocessableEntityException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -212,9 +177,7 @@ export class FailedDependencyException extends HttpException {
 		super(
 			HTTP_STATUS.FAILED_DEPENDENCY,
 			'Failed dependency',
-			'FailedDependencyException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -223,9 +186,7 @@ export class PreconditionRequiredException extends HttpException {
 		super(
 			HTTP_STATUS.PRECONDITION_REQUIRED,
 			'Precondition required',
-			'PreconditionException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -234,9 +195,7 @@ export class TooManyRequestsException extends HttpException {
 		super(
 			HTTP_STATUS.TOO_MANY_REQUESTS,
 			'Too many requests',
-			'TooManyRequestsException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -245,9 +204,7 @@ export class InternalServerErrorException extends HttpException {
 		super(
 			HTTP_STATUS.INTERNAL_SERVER_ERROR,
 			'Internal server error',
-			'InternalServerErrorException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -255,17 +212,14 @@ export class NotImplementedException extends HttpException {
 	constructor() {
 		super(
 			HTTP_STATUS.NOT_IMPLEMENTED,
-			'Not implemented',
-			'NotImplementedException',
+			'Not implemented'
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
 export class BadGatewayException extends HttpException {
 	constructor() {
-		super(HTTP_STATUS.BAD_GATEWAY, 'Bad gateway', 'BadGatewayException');
-		Object.setPrototypeOf(this, HttpException.prototype);
+		super(HTTP_STATUS.BAD_GATEWAY, 'Bad gateway');
 	}
 }
 
@@ -274,9 +228,7 @@ export class ServiceUnavailableException extends HttpException {
 		super(
 			HTTP_STATUS.SERVICE_UNAVAILABLE,
 			'Service unavailable',
-			'ServiceUnavailableException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -285,9 +237,7 @@ export class GatewayTimeoutException extends HttpException {
 		super(
 			HTTP_STATUS.GATEWAY_TIMEOUT,
 			'Gateway timeout',
-			'GatewayTimeoutException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
 
@@ -296,8 +246,6 @@ export class HttpVersionNotSupportedException extends HttpException {
 		super(
 			HTTP_STATUS.HTTP_VERSION_NOT_SUPPORTED,
 			'Http version not supported',
-			'HttpVersionNotSupportedException',
 		);
-		Object.setPrototypeOf(this, HttpException.prototype);
 	}
 }
