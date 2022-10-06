8a3535268b60588210ded4ee277761cb5d6bb3d9
talis-fb <talisonf11@gmail.com>
2022-10-05 23:44:03 -0300

    test: Finished simples cases of validate-body feature

--- spec/validate-body-decorator.spec.ts
+++ spec/validate-body-decorator.spec.ts
@@ -9,10 +9,20 @@ import { Body, Controller, Get, Post } from '../src/router/controller/mod.ts';
 
 import { IsNumber, IsString } from 'https://deno.land/x/validatte/mod.ts';
 
-// Util
+// Utils ---------------
 function jsonWithMessage(msg: string) {
 	return ({ message: msg });
 }
+async function fetchWithBody(url: string, body: any) {
+	return fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	});
+}
+// ---------------------
 
 class DTO {
 	@IsString()
@@ -43,7 +53,9 @@ class AppController {
 
 	@Post('/multiples-bodys-parameters')
 	sayHelloToThem(@Body('name') body1: DTO, @Body() body2: DTO) {
-		return jsonWithMessage(`Hello ${body1} and ${body2.name}`);
+		return jsonWithMessage(
+			`Hello ${body1} and ${body2.name}, wait... it's the same people`,
+		);
 	}
 }
 
@@ -69,37 +81,76 @@ Deno.test('Test', async (t) => {
 	const app = new DanetApplication();
 	await app.init(App);
 	const port = (await app.listen(0)).port;
+	let res, json;
 
-	// 'Return normal if body is equals DTO and 400 if it is not',
+	// ------------------------------------------------------------------
+	// STEP 1 => Return normal if body is equals DTO and 400 if it is not
+	// ------------------------------------------------------------------
+
 	// RIGHT
-	const res = await fetch(`http://localhost:${port}/test`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			name: 'James',
-			age: 25,
-		}),
+	res = await fetchWithBody(`http://localhost:${port}/test`, {
+		name: 'James',
+		age: 25, // A string as number
 	});
 	assertEquals(res.status, 200);
 	assertEquals(await res.json(), jsonWithMessage('Hello James'));
 
 	// WRONG
-	const res2 = await fetch(`http://localhost:${port}/test`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			name: 'James',
-			age: 'Potter', // A string as number
-		}),
+	res = await fetchWithBody(`http://localhost:${port}/test`, {
+		name: 'James',
+		age: 'Potter', // A string as number
 	});
-	assertEquals(res2.status, 400);
-	const json = await res2.json();
+	assertEquals(res.status, 400);
+
+	json = await res.json();
 	assertExists(json);
 	assertExists(json.reasons);
+
+	// -----------------------------------------------------------
+	// STEP 2 => Same as last one, but passing a prop in @Body(..)
+	// -----------------------------------------------------------
+
+	// WRONG
+	res = await fetchWithBody(`http://localhost:${port}/test/body-with-prop`, {
+		name: 'James',
+		age: 'Potter', // A string as number
+	});
+	assertEquals(res.status, 400);
+	json = await res.json();
+	assertExists(json);
+	assertExists(json.reasons);
+
+	// -----------------------------------------------------------------------
+	// STEP 3 => Multiples @Body, one extract a prop, and other the whole body
+	// -----------------------------------------------------------------------
+
+	// WRONG
+	res = await fetchWithBody(
+		`http://localhost:${port}/test/multiples-bodys-parameters`,
+		{
+			name: 'James',
+			age: 'Potter', // A string as number
+		},
+	);
+	assertEquals(res.status, 400);
+	json = await res.json();
+	assertExists(json);
+	assertExists(json.reasons);
+
+	// RIGHT
+	res = await fetchWithBody(
+		`http://localhost:${port}/test/multiples-bodys-parameters`,
+		{
+			name: 'James',
+			age: 25,
+		},
+	);
+	assertEquals(res.status, 200);
+	json = await res.json();
+	assertEquals(
+		json,
+		jsonWithMessage('Hello James and James, wait... it\'s the same people'),
+	);
 
 	await app.close();
 });
