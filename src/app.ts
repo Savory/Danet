7a7a21a7bf79a6303323e6b5b39eb071dd4e1622
Sorikairo <sorikairox@gmail.com>
2023-04-16 19:23:43 +0900

    feat: move Fresh logic into its own module

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-import { Application, ApplicationListenEvent, Router } from './deps.ts';
+import {
+	Application,
+	ApplicationListenEvent,
+	Middleware,
+	Router,
+} from './deps.ts';
 import { FilterExecutor } from './exception/filter/executor.ts';
 import { GuardExecutor } from './guard/executor.ts';
 import { HookExecutor } from './hook/executor.ts';
@@ -15,23 +20,6 @@ import { PossibleMiddlewareType } from './router/middleware/decorator.ts';
 import { globalMiddlewareContainer } from './router/middleware/global-container.ts';
 import { ModuleConstructor } from './module/constructor.ts';
 import { CorsOptions, oakCors } from 'https://deno.land/x/cors/mod.ts';
-import {
-	ServerContext,
-	StartOptions,
-} from 'https://deno.land/x/fresh@1.1.5/src/server/mod.ts';
-import {
-	collect,
-	generate,
-} from 'https://deno.land/x/fresh@1.1.5/src/dev/mod.ts';
-import {
-	dirname,
-	fromFileUrl,
-} from 'https://deno.land/std@0.178.0/path/mod.ts';
-
-interface ManifestData {
-	routes: string[];
-	islands: string[];
-}
 
 export class DanetApplication {
 	private app = new Application();
@@ -124,88 +112,7 @@ export class DanetApplication {
 		this.app.use(oakCors(options));
 	}
 
-	async enableFresh(url: URL, prefix: string, freshOptions: StartOptions = {}) {
-		await generateFreshManifest(url);
-		const manifest = (await import(url + './fresh.gen.ts')).default;
-		const handler = (await ServerContext.fromManifest(manifest, freshOptions))
-			.handler();
-		this.app.use(async (ctx, next) => {
-			if (
-				!ctx.request.url.toString().includes(prefix) &&
-				!ctx.request.url.toString().includes('_frsh')
-			) {
-				return await next();
-			}
-			const req = createNewRequest(ctx, prefix);
-			// deno-lint-ignore no-explicit-any
-			const res = await handler(req, null as any);
-			ctx.response.body = res.body;
-			ctx.response.status = res.status;
-			ctx.response.headers = res.headers;
-		});
+	use(middleware: Middleware) {
+		this.app.use(middleware);
 	}
-
-	async enableFreshOnRoot(
-		url: URL,
-		prefix: string,
-		freshOptions: StartOptions = {},
-	) {
-		await generateFreshManifest(url);
-		const manifest = (await import(url + './fresh.gen.ts')).default;
-		const handler = (await ServerContext.fromManifest(manifest, freshOptions))
-			.handler();
-		this.danetRouter.setPrefix(prefix);
-		this.app.use(async (ctx, next) => {
-			if (ctx.request.url.toString().includes(prefix)) {
-				return await next();
-			}
-			const req = createNewRequest(ctx, '');
-			// deno-lint-ignore no-explicit-any
-			const res = await handler(req, null as any);
-			ctx.response.body = res.body;
-			ctx.response.status = res.status;
-			ctx.response.headers = res.headers;
-		});
-	}
-}
-
-async function generateFreshManifest(url: URL) {
-	const fileUrl = fromFileUrl(url);
-	const dir = dirname(fileUrl + 'fakefile');
-	let currentManifest: ManifestData;
-	const prevManifest = Deno.env.get('FRSH_DEV_PREVIOUS_MANIFEST');
-	if (prevManifest) {
-		currentManifest = JSON.parse(prevManifest);
-	} else {
-		currentManifest = { islands: [], routes: [] };
-	}
-	const newManifest = await collect(dir);
-	Deno.env.set('FRSH_DEV_PREVIOUS_MANIFEST', JSON.stringify(newManifest));
-
-	const manifestChanged =
-		!arraysEqual(newManifest.routes, currentManifest.routes) ||
-		!arraysEqual(newManifest.islands, currentManifest.islands);
-
-	if (manifestChanged) await generate(dir, newManifest);
-}
-
-function createNewRequest<AS>(ctx: HttpContext, prefix: string) {
-	let newUrl = ctx.request.url.toString().replace(prefix, '');
-	if (newUrl.endsWith('/')) {
-		newUrl = newUrl.slice(0, -1);
-	}
-	const req = new Request(newUrl, {
-		body: ctx.request.originalRequest.getBody().body,
-		headers: ctx.request.headers,
-		method: ctx.request.method,
-	});
-	return req;
-}
-
-function arraysEqual<T>(a: T[], b: T[]): boolean {
-	if (a.length !== b.length) return false;
-	for (let i = 0; i < a.length; ++i) {
-		if (a[i] !== b[i]) return false;
-	}
-	return true;
 }
