cf3ccfbce19165a82a9c29b7329cddef4291a3dd
Sorikairo <sorikairox@gmail.com>
2023-07-03 22:55:10 +0900

    chore: downgrade handlebar to remove node: specifier

--- src/renderer/handlebar.ts
+++ src/renderer/handlebar.ts
@@ -1,5 +1,5 @@
 import { Renderer } from './interface.ts';
-import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
+import { Handlebars } from 'https://deno.land/x/handlebars@v0.8.0/mod.ts';
 
 const defaultOption = {
 	baseDir: 'views',
