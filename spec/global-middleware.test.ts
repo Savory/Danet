77ffc670735db2f2da50e417dd1c21c5d2fbfaa1
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2023-12-26 14:48:26 +0000

    feat: cascade middleware return value

--- spec/global-middleware.test.ts
+++ spec/global-middleware.test.ts
@@ -3,6 +3,18 @@ import { Module } from '../src/module/decorator.ts';
 import { assertEquals } from '../src/deps_test.ts';
 import { Controller, Get } from '../src/router/controller/decorator.ts';
 import { Post } from '../mod.ts';
+import { Logger } from '../mod.ts';
+import { HttpContext, NextFunction } from '../mod.ts';
+
+const logger = new Logger('Logger');
+const loggerMiddleware = async (
+	ctx: HttpContext,
+	next: NextFunction,
+  ) => {
+	logger.log(`[Start]${ctx.req.method} - ${ctx.req.routePath}`);
+	return await next();
+  };
+  
 
 class Todo {
 	constructor(private name: string, private description: string) {
@@ -11,11 +23,13 @@ class Todo {
 }
 const app = new DanetApplication();
 app.enableCors();
+app.addGlobalMiddlewares(loggerMiddleware);
 
 @Controller('todo')
 class MyController {
 	@Get('')
 	async simpleGet() {
+		logger.log('Simple get');
 		return new Todo('test', 'ok');
 	}
 
