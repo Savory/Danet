a59a12afd685ea1a9db137f9c077640690bd4a20
Marco Antônio <ma.souza.junior@gmail.com>
2024-02-14 21:42:07 -0300

    enh: add code example for events

---
+++ example/events.ts
@@ -0,0 +1,54 @@
+import {
+	Controller,
+	DanetApplication,
+	EventEmitter,
+	Injectable,
+	Module,
+	OnEvent,
+	Post,
+} from '../mod.ts';
+
+type User = {};
+
+@Injectable()
+class UserListeners {
+	@OnEvent('new-user')
+	notifyUser(user: User) {
+		console.log('new user created', user);
+	}
+
+	@OnEvent('new-user')
+	async sendWelcomeEmail(user: User) {
+		console.log('send email', user);
+	}
+}
+
+@Controller('user')
+class UserController {
+	constructor(
+		private eventEmitter: EventEmitter<User>,
+	) {}
+
+	@Post()
+	// deno-lint-ignore no-explicit-any
+	create(@Body() body: any) {
+		const user: User = {};
+		this.eventEmitter.emmit('new-user', user);
+		return JSON.stringify(user);
+	}
+}
+
+@Module({
+	controllers: [UserController],
+	injectables: [UserListeners],
+})
+class AppModule {}
+
+const app = new DanetApplication();
+await app.init(AppModule);
+
+let port = Number(Deno.env.get('PORT'));
+if (isNaN(port)) {
+	port = 3000;
+}
+app.listen(port);
