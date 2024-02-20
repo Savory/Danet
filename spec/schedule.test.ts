361a560931a21689f0210aeeb54586360a8064dc
Marco Antônio <ma.souza.junior@gmail.com>
2024-02-20 13:17:06 -0300

    enh: add tests for @Interval

--- spec/schedule.test.ts
+++ spec/schedule.test.ts
@@ -9,12 +9,7 @@ import {
 	Timeout,
 } from '../mod.ts';
 import { assertEquals } from '../src/deps_test.ts';
-import {
-	assertSpyCall,
-	assertSpyCallArg,
-	FakeTime,
-	spy,
-} from '../src/deps_test.ts';
+import { assertSpyCallArg, FakeTime, spy } from '../src/deps_test.ts';
 
 Deno.test('Schedule Module', async (t) => {
 	const cron = Deno.cron;
@@ -25,7 +20,6 @@ Deno.test('Schedule Module', async (t) => {
 		@Cron(CronExpression.EVERY_MINUTE)
 		runEachMinute() {}
 	}
-	// TODO: add tests for @Interval & @Timeout
 
 	@Module({
 		imports: [ScheduleModule],
@@ -75,6 +69,41 @@ Deno.test('Timeout Module', async (t) => {
 
 		await time.tickAsync(IntervalExpression.MILISECOND);
 		assertEquals(cb.calls.length, 1);
+	});
+
+	await application.close();
+});
+
+Deno.test('Interval Module', async (t) => {
+	const time = new FakeTime();
+	const cb = spy();
+
+	class TestListener {
+		@Interval(IntervalExpression.MILISECOND)
+		runEachMinute() {
+			cb();
+		}
+	}
+
+	@Module({
+		imports: [ScheduleModule],
+		injectables: [TestListener],
+	})
+	class TestModule {}
+
+	const application = new DanetApplication();
+	await application.init(TestModule);
+	await application.listen(0);
+
+	await t.step('should be called once after tick', async () => {
+		await time.tickAsync(IntervalExpression.MILISECOND);
+		assertEquals(cb.calls.length, 1);
+
+		await time.tickAsync(IntervalExpression.MILISECOND);
+		assertEquals(cb.calls.length, 2);
+
+		await time.tickAsync(IntervalExpression.MILISECOND);
+		assertEquals(cb.calls.length, 3);
 	});
 
 	await application.close();
