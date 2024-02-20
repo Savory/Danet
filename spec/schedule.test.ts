61af1f92c077f48c7011a883c564b0a886adacf0
Marco Antônio <ma.souza.junior@gmail.com>
2024-02-20 13:15:49 -0300

    enh: add tests for @Timeout

--- spec/schedule.test.ts
+++ spec/schedule.test.ts
@@ -1,16 +1,20 @@
 import {
-	assertSpyCall,
-	assertSpyCallArg,
-	spy,
-} from 'https://deno.land/std@0.135.0/testing/mock.ts';
-import {
 	Cron,
 	CronExpression,
 	DanetApplication,
+	Interval,
 	IntervalExpression,
 	Module,
 	ScheduleModule,
+	Timeout,
 } from '../mod.ts';
+import { assertEquals } from '../src/deps_test.ts';
+import {
+	assertSpyCall,
+	assertSpyCallArg,
+	FakeTime,
+	spy,
+} from '../src/deps_test.ts';
 
 Deno.test('Schedule Module', async (t) => {
 	const cron = Deno.cron;
@@ -42,4 +46,36 @@ Deno.test('Schedule Module', async (t) => {
 
 	await application.close();
 	Deno.cron = cron;
+});
+
+Deno.test('Timeout Module', async (t) => {
+	const time = new FakeTime();
+	const cb = spy();
+
+	class TestListener {
+		@Timeout(IntervalExpression.MILISECOND)
+		runEachMinute() {
+			cb();
+		}
+	}
+
+	@Module({
+		imports: [ScheduleModule],
+		injectables: [TestListener],
+	})
+	class TestModule {}
+
+	const application = new DanetApplication();
+	await application.init(TestModule);
+	await application.listen(0);
+
+	await t.step('should be called once after tick', async () => {
+		await time.tickAsync(IntervalExpression.MILISECOND);
+		assertEquals(cb.calls.length, 1);
+
+		await time.tickAsync(IntervalExpression.MILISECOND);
+		assertEquals(cb.calls.length, 1);
+	});
+
+	await application.close();
 });
