cde9054d38b4b39c80bf26456b85e7073ef9d3fb
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2024-10-02 13:04:59 +0000

    docs: add metadata JSDOC

--- src/metadata/decorator.ts
+++ src/metadata/decorator.ts
@@ -1,6 +1,19 @@
+/**   
+** metadataDecorator
+** Provides decorator to set Metadata
+** @module
+*/
+
 import { ControllerConstructor } from '../router/controller/constructor.ts';
 import { MetadataHelper } from './helper.ts';
 
+/**
+ * A function type that represents a metadata decorator.
+ *
+ * @param target - The target object or constructor to which the metadata is applied.
+ * @param propertyKey - An optional property key for the target.
+ * @param descriptor - An optional property descriptor for the target.
+ */
 export type MetadataFunction =  (
 	// deno-lint-ignore ban-types
 	target: ControllerConstructor | Object,
@@ -9,6 +22,13 @@ export type MetadataFunction =  (
 	descriptor?: TypedPropertyDescriptor<any>,
 ) => void;
 
+/**
+ * Sets metadata on the target object or method.
+ *
+ * @param key - The key for the metadata.
+ * @param value - The value for the metadata.
+ * @returns a MetadaFunction.
+ */
 export const SetMetadata = (key: string, value: unknown): MetadataFunction =>
 (
 	// deno-lint-ignore ban-types
--- src/metadata/helper.ts
+++ src/metadata/helper.ts
@@ -1,12 +1,39 @@
+/* 
+** metadata
+** Provides metadata helper functions
+** Dependencies: Reflect
+** @module
+*/
+
 import { Reflect } from '../deps.ts';
 
 export class MetadataHelper {
+
+	/**
+	 * Determines if the provided value is an object.
+	 *
+	 * This function checks if the given value is of type 'object' and not null,
+	 * or if it is of type 'function'.
+	 *
+	 * @template T - The type of the value being checked.
+	 * @param x - The value to check. It can be of type T, undefined, null, boolean, string, symbol, or number.
+	 * @returns A boolean indicating whether the value is an object.
+	 */
 	static IsObject<T>(
 		x: T | undefined | null | boolean | string | symbol | number,
 	): x is T {
 		return typeof x === 'object' ? x !== null : typeof x === 'function';
 	}
 
+	/**
+	 * Retrieves metadata of a specified key from an object or its property.
+	 *
+	 * @template T - The expected type of the metadata value.
+	 * @param {string} key - The key for the metadata to retrieve.
+	 * @param {unknown} obj - The target object from which to retrieve the metadata.
+	 * @param {string | symbol} [property] - Optional. The property of the object from which to retrieve the metadata.
+	 * @returns {T} - The metadata value associated with the specified key.
+	 */
 	static getMetadata<T>(
 		key: string,
 		obj: unknown,
@@ -18,6 +45,17 @@ export class MetadataHelper {
 		return Reflect.getMetadata(key, obj) as T;
 	}
 
+	/**
+	 * Sets metadata for a target object or its property.
+	 *
+	 * @param key - The metadata key.
+	 * @param value - The metadata value.
+	 * @param target - The target object to set the metadata on.
+	 * @param property - Optional. The property of the target object to set the metadata on.
+	 * If not provided, the metadata is set on the target object itself.
+	 * 
+	 * @returns void
+	 */
 	static setMetadata(
 		key: string,
 		value: unknown,
