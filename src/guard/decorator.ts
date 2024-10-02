bc7bdcaec153df8cf4482ae2a041e56417ca3b2d
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2024-10-02 14:21:19 +0000

    docs: guard

--- src/guard/decorator.ts
+++ src/guard/decorator.ts
@@ -2,6 +2,14 @@ import { Constructor } from '../utils/constructor.ts';
 import { MetadataFunction, SetMetadata } from '../metadata/decorator.ts';
 
 export const guardMetadataKey = 'authGuards';
+/**
+ * Applies a guard to a route handler or controller.
+ * 
+ * https://danet.land/overview/guards.html
+ * 
+ * @param guard - The constructor of the guard to be applied.
+ * @returns A function that sets the metadata for the guard.
+ */
 export function UseGuard(guard: Constructor): MetadataFunction {
 	return SetMetadata(guardMetadataKey, guard);
 }
