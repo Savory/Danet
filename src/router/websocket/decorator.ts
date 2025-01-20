07ad803a1a8497cc50ec8a1f8b9f3af1e4514387
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2025-01-20 17:58:54 +0900

    Update decorator.ts

--- src/router/websocket/decorator.ts
+++ src/router/websocket/decorator.ts
@@ -1,9 +1,9 @@
-import { SetMetadata } from '../../metadata/decorator.ts';
+import { MetadataFunction, SetMetadata } from '../../metadata/decorator.ts';
 
-export function WebSocketController(endpoint = ''): MethodDecorator {
+export function WebSocketController(endpoint = ''): MetadataFunction {
 	return SetMetadata('websocket-endpoint', endpoint);
 }
 
-export function OnWebSocketMessage(topic: string): MethodDecorator {
+export function OnWebSocketMessage(topic: string): MetadataFunction {
 	return SetMetadata('websocket-topic', topic);
 }
