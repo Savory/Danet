fb373ebcb026fdf1def7a7cfc13878a08a48495b
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2025-01-20 17:51:35 +0900

    Update mod.ts

--- src/router/mod.ts
+++ src/router/mod.ts
@@ -5,3 +5,4 @@ export * from './utils.ts';
 export * from './controller/mod.ts';
 export * from './middleware/mod.ts';
 export * from './websocket/decorator.ts';
+export * from './websocket/payload.ts';
